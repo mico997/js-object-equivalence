f387df38d281cdf0c60c1246d0b9fb9b14965a9f
Mike-Vidal <113799147+Mike-Vidal@users.noreply.github.com>
2023-09-17 14:14:02 -0400

    Added function declaration, Objects for comparison and callback function

--- object-equivalence.js
+++ object-equivalence.js
@@ -1,21 +1,34 @@
-//Program Implementation outline!
+/*Program Implementation outline!
+Build out Function expression to accepts 2 object params
+*/
+const isEqual = (obj1, obj2) => {
+    const obj1Keys = Object.keys(obj1);
+    const obj2Keys = Object.keys(obj2);
+}
 
-//Build out Function expression to accepts 2 object params
 
 
-   
 
-  //Run logic to traverse object and compare values
+
+//Run logic to traverse object and compare values
 
 
 
 //Declare Obj1
-
-
+const obj1 = {
+    name: "Cornelia",
+    age: 38
+}
 
 //Declare Object2
+const obj2 = {
+    name: "Yariza",
+    age: 23
+}
 
 
-
+//Using strict comparison to determine value = false
+obj1 === obj2
 
 //Run callback Function
+isEqual(obj1, obj2);
